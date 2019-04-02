eee741253103611f19f992bf4fc336bff67da988
Schierke1 <36467193+Schierke0@users.noreply.github.com>
2019-04-02 02:03:52 -0600

    insert table Suppliers

---
+++ Insert supplier.sql
@@ -0,0 +1,292 @@
+USE [Northwind]
+GO
+
+INSERT INTO [dbo].[Suppliers]
+           ([CompanyName]
+           ,[ContactName]
+           ,[ContactTitle]
+           ,[Address]
+           ,[City]
+           ,[Region]
+           ,[PostalCode]
+           ,[Country]
+           ,[Phone]
+           ,[Fax]
+           ,[HomePage])
+     VALUES
+           ('Corporación Dinant'
+           ,'FernandoArgeñal' 
+           ,'Gerente General'
+           ,'Frente a Plantas Tropicales, Boulevard Suyapa' 
+           ,'Tegucigalpa'
+           ,'norte'
+           ,'5768'
+           ,'Honduras' 
+           ,'2275-3370'
+           ,'6783294'
+           ,' ' ),
+
+		    ('Alimento S. de R. L.'
+           ,'Lynda Marín' 
+           ,'Sub-gerente de ventas'
+           ,' 5 Avenida, Tegucigalpa' 
+           ,'Tegucigalpa'
+           ,'centro'
+           ,'4745756'
+           ,'Honduras' 
+           ,' 2237-2125'
+           ,'6876000'
+           ,' ' ),
+
+		   ('Distribuidora Istmania S.A. de C.V.'
+           ,'Juan CarlosGarcía' 
+           ,'Consultor'
+           ,'Tegucigalpa' 
+           ,'Tegucigalpa'
+           ,'sur'
+           ,'290053644'
+           ,'Honduras' 
+           ,'2240-0500'
+           ,'654740'
+           ,' ' ),
+
+		('Industrial de Alimentos'
+           ,'Iris Cuellar' 
+           ,'Abastecimiento'
+           ,'Tegucigalpa' 
+           ,'Tegucigalpa'
+           ,'centro'
+           ,'290053644'
+           ,'Honduras' 
+           ,'2245-6237'
+           ,'850'
+           ,' ' ),
+
+		   ('Distribuidora Istmania S.A. de C.V.'
+           ,'Jessica Ham' 
+           ,'Gerente de
+Importaciones'
+           ,'Av Los Próceres, Tegucigalpa' 
+           ,'Tegucigalpa'
+           ,'boulevar'
+           ,'233353644'
+           ,'Honduras' 
+           ,'2221-0857'
+           ,'65654748'
+           ,' ' ),
+
+		   ('Supermercado La Colonia'
+           ,'Gabriel Hernández' 
+           ,'Consultor'
+           ,'Central de
+Abasto,
+San Pedro Sula' 
+           ,'SPS'
+           ,'norte'
+           ,'290053644'
+           ,'Honduras' 
+           ,'9644-7594'
+           ,'65858'
+           ,' ' ),
+
+		   ('INALMA '
+           ,'Juan CarlosGarcía' 
+           ,'Gerente de
+Importaciones'
+           ,'Cortes' 
+           ,'San Pedro Sula'
+           ,'norte'
+           ,'2944444'
+           ,'Honduras' 
+           ,'2557‐5232'
+           ,'65473440'
+           ,' ' ),
+
+		   ('Banco de Alimentos de Honduras'
+           ,'Ing. Luis O. Yip' 
+           ,'Asistente Gerencia'
+           ,'calle real' 
+           ,'Tegucigalpa'
+           ,'sur'
+           ,'2900554'
+           ,'Honduras' 
+           ,'9990-2178'
+           ,'6566'
+           ,' ' ),
+
+		   ('Los Chiveros SPR'
+           ,'René Padilla' 
+           ,'Comercialización al
+por menor'
+           ,'olancho' 
+           ,'catacamas'
+           ,'sur'
+           ,'290445544'
+           ,'Honduras' 
+           ,'2545-4500'
+           ,'88740'
+           ,' ' ),
+
+		   ('3 Mega Service'
+           ,'Juan Carlos
+Monge
+' 
+           ,'Consultor'
+           ,'santa cruz' 
+           ,'san salvador'
+           ,'noreste'
+           ,'290033'
+           ,'El Salvador' 
+           ,'7987 9296 '
+           ,'605433'
+           ,' ' ),
+
+		   ('Montecito
+Mercantile
+Company
+'
+           ,'Jairo Javier
+Medina' 
+           ,'SubGerente'
+           ,'seattle' 
+           ,'seattle'
+           ,'sur'
+           ,'04'
+           ,'EEUU' 
+           ,'813 474
+5678
+'
+           ,'655'
+           ,' ' ),
+
+		   ('KINAGRO '
+           ,'Juan CarlosGarcía' 
+           ,'Consultor'
+           ,'Cortes ' 
+           ,'San Pedro Sula'
+           ,'sur'
+           ,'29044'
+           ,'Honduras' 
+           ,'2243-3560'
+           ,'7725'
+           ,' ' ),
+
+		   ('0 Industrias Sula'
+           ,'Lolita Lopez' 
+           ,'Sub-gerente de
+Compras
+'
+           ,'Cortes' 
+           ,'San Pedro Sula'
+           ,'norte'
+           ,'290053644'
+           ,'Honduras' 
+           ,'2559-3280'
+           ,'8347'
+           ,' ' ),
+
+		   ('El Gran Sabor'
+           ,'Juan CarlosGarcía' 
+           ,'Consultor'
+           ,'Francisco
+Morazán' 
+           ,'Tegucigalpa'
+           ,'centro'
+           ,'2934625554'
+           ,'Honduras' 
+           ,'2238-1911 '
+           ,'6548979'
+           ,' ' ),
+
+		   ('Honduras Rice S.A'
+           ,'Jorge Thompson' 
+           ,'Gerente General'
+           ,'Francisco
+Morazán' 
+           ,'Tegucigalpa'
+           ,'norteste'
+           ,'0306463644'
+           ,'Honduras' 
+           ,'2239-1512'
+           ,'4655'
+           ,' ' ),
+   (' BEPROAGRO '
+           ,'Juan CarlosGarcía' 
+           ,'Consultor'
+           ,'Cortes' 
+           ,'Villa Nueva'
+           ,'norte'
+           ,'2444'
+           ,'Honduras' 
+           ,'2239-0425'
+           ,'6559'
+           ,' ' ),
+
+		   (' La Copaneca '
+           ,'Juan CarlosGarcía' 
+           ,'supervisor'
+           ,'Francisco
+Morazán' 
+           ,'Tegucigalpa'
+           ,'centro'
+           ,'2424'
+           ,'Honduras' 
+           ,'2289-4881
+'
+           ,'789940'
+           ,' ' ),
+
+		   ('Bodega Consuelo '
+           ,' José Rene Rivera ' 
+           ,'Propietario'
+           ,'vila vieja' 
+           ,'Santa Bárbara '
+           ,'almacenes'
+           ,'26674'
+           ,'Honduras' 
+           ,'9459‐5889'
+           ,'654620'
+           ,' ' ),
+
+		   ('Bodega Mirna '
+           ,'Raúl Amaya ' 
+           ,'Propietario'
+           ,'La Entrada ' 
+           ,'Copan '
+           ,'mercado'
+           ,'03644'
+           ,'Honduras' 
+           ,'9835‐9979'
+           ,'65094280'
+           ,' ' ),
+
+		   ('Beneficio DIECK '
+           ,'Lic. Marco Alvarado' 
+           ,'JeFe Mercadeo'
+           ,'Tegucigalpa' 
+           ,'Tegucigalpa'
+           ,'centro'
+           ,'553644'
+           ,'Honduras' 
+           ,'9652-0576'
+           ,'982240'
+           ,' ' ),
+		   ('RANA '
+           ,'Guillermo Carrasco' 
+           ,'Jefe Ventas'
+           ,'corralio' 
+           ,'Santa Cruz'
+           ,'este'
+           ,'2944'
+           ,'Honduras' 
+           ,'2232-6360'
+           ,'6395'
+           ,' ' )
+
+GO
+
+
+
+
+
+
