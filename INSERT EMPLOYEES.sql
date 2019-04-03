b4b7481aec79751edf862b72a237dccef7201a7c
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2019-04-03 16:42:15 -0600

    backup
    backup de la base de datos implementando los nuevos empleados y proveedores

--- INSERT EMPLOYEES.sql
+++ INSERT EMPLOYEES.sql
@@ -13,7 +13,7 @@ CREATE OR ALTER PROC Insertar
 AS
 	WHILE @count < 41 
 	BEGIN
-		SET @option = ROUND(((10-1) * RAND() + 1), 0);
+		SET @option = ROUND(((20-1) * RAND() + 1), 0);
 		SET @nombre = CASE 
 						when @option = 1 then 'Ramon' 
 						when @option = 2 then 'Jair'
@@ -25,10 +25,21 @@ AS
 						when @option = 8 then 'Lester'
 						when @option = 9 then 'Oscar'
 						when @option = 10 then 'German'
+						when @option = 11 then 'Steve'
+						when @option = 12 then 'John'
+						when @option = 13 then 'Joe'
+						when @option = 14 then 'Walter'
+						when @option = 15 then 'Kratos'
+						when @option = 16 then 'Naruto'
+						when @option = 17 then 'Kakaroto'
+						when @option = 18 then 'Bulma'
+						when @option = 19 then 'Alison'
+						when @option = 20 then 'Acza'
+
 						else 'Raul'
 
 					END;
-		SET @option = ROUND(((10-1) * RAND() + 1), 0);
+		SET @option = ROUND(((20-1) * RAND() + 1), 0);
 
 		SET @apellido = CASE 
 						when @option = 1 then 'Ramirez' 
@@ -41,12 +52,22 @@ AS
 						when @option = 8 then 'Alvarez'
 						when @option = 9 then 'Alvarado'
 						when @option = 10 then 'Tejada'
+						when @option = 11 then 'Jobs'
+						when @option = 12 then 'Snow'
+						when @option = 13 then 'Morales'
+						when @option = 14 then 'White'
+						when @option = 15 then 'Ortega'
+						when @option = 16 then 'Nilo'
+						when @option = 17 then 'Uchiha'
+						when @option = 18 then 'Rogers'
+						when @option = 19 then 'Carranza'
+						when @option = 20 then 'Garcia'
 						else 'Zelaya'
 
 					END;
 		SET @option = ROUND(((5-1) * RAND() + 1), 0);
 		SET @lugar = CASE 
-						when @option = 1 then 'Francisco Morazan' 
+						when @option = 1 then 'Francisco Morazan'
 						when @option = 2 then 'Cortes'
 						when @option = 3 then 'Atlantida'
 						when @option = 4 then 'Comayagua'
@@ -65,12 +86,12 @@ AS
 		SET @cargo = CASE 
 						when @option = 1 then 'Sales Man' 
 						when @option = 2 then 'Operative Manager'
-						when @option = 3 then 'concierge'
+						when @option = 3 then 'Concierge'
 						when @option = 4 then 'Security Man'
 						else 'RRHH'
 
 					END;
-		INSERT INTO Employees (LastName, FirstName, Title, TitleOfCourtesy, BirthDate, HireDate, [Address], City, Region, PostalCode, Country, HomePhone, Extension, Photo, Notes, ReportsTo, PhotoPath)
+		INSERT INTO Employees (LastName, FirstName, Title, TitleOfCourtesy, BirthDate, HireDate, City, Region, PostalCode, HomePhone, Extension, Photo, Notes, ReportsTo, PhotoPath)
 		VALUES (@apellido, @nombre, @cargo, 'Mr',convert(datetime,'18-06-18 10:34:09 PM',5), convert(datetime,'18-06-18 10:34:09 PM',5), @lugar, @ciudad, 'WA', 11011, 'HN', '123456789', 4567, 'PHOTO','Graduated', 2, 'http://accweb/emmployees/davolio.bmp');
 		SET @count =  @count + 1;
 	END;
