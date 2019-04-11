2d86fcaafdd181a8b0d4e499cffd2d1d3361e229
hll3gt14 <37005783+hll3gt14@users.noreply.github.com>
2019-04-10 21:26:40 -0600

    Add files via upload

---
+++ Scripts/ETL/SQL Products.sql
@@ -0,0 +1,14 @@
+
+SELECT ProductID,
+      ProductName,
+      SupplierID,
+      CategoryID,
+      QuantityPerUnit,
+      UnitPrice,
+      UnitsInStock,
+      UnitsOnOrder,
+      ReorderLevel,
+      Discontinued
+  FROM Products;
+
+
